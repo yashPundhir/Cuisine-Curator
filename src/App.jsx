7d97c2782be03bbdedfa7ea573f15f5134fecf0f
yashPundhir <yash.pundhir.prof@gmail.com>
2023-04-21 23:01:42 +0530

    Navbar component added to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
 	return (
 		<>
-			<h1 className="text-3xl text-rose-400 capitalize">Hello World</h1>
+			<Navbar />
 		</>
 	);
 }
