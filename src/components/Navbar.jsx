dffb6740c6470c923e2028a486829ea4b820d13e
yashPundhir <yash.pundhir.prof@gmail.com>
2023-04-21 23:00:39 +0530

    Navbar component completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,19 @@
-import logo from "../assets/food.webp";
+import logo from "../assets/logo.webp";
+
 const Navbar = () => {
 	return (
-		<div className="py-2">
-			<nav className="relative px-4 py-4 flex justify-between items-center border-y border-gray-400 dark:border-gray-700">
-				<a className="text-3xl font-bold leading-none" href="#">
-					<img className="w-12 h-16" src={logo} alt="" />
-					{/* <svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 24 24"
-						fill="#4F46E5"
-						className="w-8 h-8"
-					>
-						<path
-							fillRule="evenodd"
-							d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 01.75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 019.75 22.5a.75.75 0 01-.75-.75v-4.131A15.838 15.838 0 016.382 15H2.25a.75.75 0 01-.75-.75 6.75 6.75 0 017.815-6.666zM15 6.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"
-							clipRule="evenodd"
-						/>
-						<path d="M5.26 17.242a.75.75 0 10-.897-1.203 5.243 5.243 0 00-2.05 5.022.75.75 0 00.625.627 5.243 5.243 0 005.022-2.051.75.75 0 10-1.202-.897 3.744 3.744 0 01-3.008 1.51c0-1.23.592-2.323 1.51-3.008z" />
-					</svg> */}
+		<div className="pb-10 pt-2">
+			<nav className="relative px-8 py-0 flex justify-between items-center border-y border-gray-400 dark:border-gray-700">
+				<a
+					className="text-3xl font-bold leading-none flex items-center space-x-4"
+					href="#"
+				>
+					<span>
+						<img className="w-24 h-24" src={logo} alt="" />
+					</span>
+					<span className="text-gray-300 dark:text-gray-300 text-xl">
+						CUISINE CURATOR
+					</span>
 				</a>
 				<div className="lg:hidden">
 					<button className="navbar-burger flex items-center text-gray-600 dark:text-gray-300 p-3">
@@ -34,54 +30,56 @@ const Navbar = () => {
 				<ul className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6 lg:gap-10">
 					<li>
 						<a
-							className="text-2xl text-gray-500 hover:text-gray-600 dark:text-gray-300"
+							className="text-2xl text-rose-400 hover:text-rose-600 font-bold"
 							href="#"
 						>
 							Home
 						</a>
 					</li>
-
-					<li>
-						<a className="text-2xl text-indigo-600 font-bold" href="#">
-							About Us
-						</a>
-					</li>
-
 					<li>
 						<a
-							className="text-2xl text-gray-500 hover:text-gray-600 dark:text-gray-300"
+							className="text-2xl text-gray-300 hover:text-gray-500 dark:text-gray-300"
 							href="#"
 						>
-							Services
+							Ingredients
 						</a>
 					</li>
-
 					<li>
 						<a
-							className="text-2xl text-gray-500 hover:text-gray-600 dark:text-gray-300"
+							className="text-2xl text-gray-300 hover:text-gray-500 dark:text-gray-300"
 							href="#"
 						>
-							Pricing
-						</a>
-					</li>
-
-					<li>
-						<a
-							className="text-2xl text-gray-500 hover:text-gray-600 dark:text-gray-300"
-							href="#"
-						>
-							Contact
+							Meals
 						</a>
 					</li>
 				</ul>
-				<div className="space-x-2 hidden lg:block">
-					<button className="rounded-md border border-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-indigo-600 hover:bg-indigo-500 ">
-						Login
-					</button>
-					<button className="rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white hover:bg-indigo-500 ">
-						SignUp
-					</button>
-				</div>
+				<ul className="hidden lg:flex lg:items-center lg:justify-end grow mr-4">
+					<li>
+						<div className="relative border-[1.5px] border-rose-500 rounded-lg p-1">
+							<span className="absolute inset-y-0 left-0 flex items-center pl-3">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									strokeWidth={1.5}
+									stroke="currentColor"
+									className="w-4 h-4 text-gray-500"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+									/>
+								</svg>
+							</span>
+							<input
+								type="text"
+								className="py-2 pl-10 pr-4 text-sm text-gray-700 bg-gray-100 dark:bg-gray-800 rounded-md focus:outline-none focus:bg-white focus:text-gray-900 "
+								placeholder="Search for a Meal..."
+							/>
+						</div>
+					</li>
+				</ul>
 			</nav>
 		</div>
 	);
